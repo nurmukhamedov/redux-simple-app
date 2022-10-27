7c4264b50e649306cc5ad2e8144aa3e292ee44a3
nurmukhamedov <nurmuxamedovabdurasul@gmail.com>
2022-10-27 20:22:25 +0500

    redux-simple-app

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,33 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = [
+  { id: "1", title: "First Post!", content: "Hello!" },
+  { id: "2", title: "Second Post", content: "More text" },
+];
+
+const postsSlice = createSlice({
+  name: "posts",
+  initialState,
+  reducers: {
+    postAdded: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare(title, content) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content,
+          },
+        };
+      },
+    },
+  },
+});
+
+export const selectAllPosts = state => state.posts;
+
+export const { postAdded } = postsSlice.actions;
+
+export default postsSlice.reducer;
